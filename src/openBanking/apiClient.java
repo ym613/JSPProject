3335e5cdc9e63e836b0db6bd5855442bb6e4fbc4
ym613 <yunmin716@naver.com>
2020-11-15 10:53:55 +0900

    openbanking commit

---
+++ src/openBanking/apiClient.java
@@ -0,0 +1,20 @@
+package openBanking;
+
+public class apiClient {
+	private static String client_id = "s585VW6D37qX4Mmq53s69y1M9U3q6azQ8tgjx7Ha";
+	private static String client_secret = "jn6XL0HxQQK2f19KeDoxkBtPrRubQR4eQd51lN08";
+	private static String client_use_code = "T991648740";
+
+	public static String getId() {
+		return client_id;
+	}
+
+	public static String getSecret() {
+		return client_secret;
+	}
+
+	public static String getCode() {
+		return client_use_code;
+	}
+
+}
